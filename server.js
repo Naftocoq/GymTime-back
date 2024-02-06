24f86f29af15df17a93efd0da2225c46829e1a08
Naftocoq <mohamed.nafti@outlook.com>
2024-02-06 20:46:05 +0100

    uploading back

---
+++ server.js
@@ -0,0 +1,43 @@
+require('dotenv').config()
+
+const express = require('express')
+const mongoose = require('mongoose')
+const workoutRoutes = require('./routes/workouts')
+const userRoutes = require('./routes/user')
+
+
+// express app
+const app = express()
+
+// middleware
+app.use(express.json())
+
+app.use((req, res, next) => {
+  console.log(req.path, req.method)
+  next()
+})
+
+// routes
+app.use('/api/workouts', workoutRoutes)
+app.use('/api/user', userRoutes)
+
+// render
+app.use((req, res) => {
+  res.send(' Youpi, API is running ! ')
+})
+
+const PORT = process.env.PORT || 6489 
+//
+
+// connect to db
+mongoose.connect(process.env.MONGO_URI)
+  .then(() => {
+    console.log('connected to database')
+    // listen to port
+    app.listen(process.env.PORT, () => {
+      console.log('listening for requests on port', process.env.PORT)
+    })
+  })
+  .catch((err) => {
+    console.log(err)
+  }) 
